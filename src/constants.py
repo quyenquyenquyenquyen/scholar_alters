c29b22eab555dadd23825809d3c6c0075bffb5bd
manh-td <dmtran120901@gmail.com>
2025-04-08 10:21:16 +0800

    Update constants.py

--- src/constants.py
+++ src/constants.py
@@ -5,112 +5,105 @@ SCOPES = [
 ]
 CLIENTSECRETS_LOCATION = "./credentials.json"
 
-SOFTWARE_VUL_PATTERN = [
-    "Software Vulnerabilities",
-    "Vulnerabilities",
-    "Vulnerability",
-]
-SOFTWARE_VERIFY_PATTERN = [
-    "Formal Verification",
-    "Verification",
-]
-SMART_CONTRACTS_PATTERN = [
-    "Smart Contracts",
-    "Smart Contract",
-]
-UNIT_TEST_PATTERN = [
-    "Unit Test"
-]
-APR_PATTERN = [
-    "Automated Program Repair",
-    "Program Repair",
-    "Automated Repair",
-]
-LLMS_PATTERN = [
-    "LLMs",
-    "LLM",
-    "Large Language Models",
-    "Large Language Model",
-    "ChatGPT",
-    "GPT"
-]
-FUZZING_PATTERN = [
-    "Fuzzing",
-    "Fuzz",
-]
-CODE_REVIEW_PATTERN = [
-    "Code Review",
-]
-CI_PATTERN = [
-    "Continuous Integration"
-]
-FL_PATTERN = [
-    "Fault Localization"
-]
-CODE_PATTERN = [
-    "Code"
-]
-DEFECT_PATTERN = [
-    "Defect"
-]
-BUG_PATTERN = [
-    "Bug"
-]
-COMMIT_MESSAGE_PATTERN = [
-    "Commit Message",
-    "Commit Messages",
-]
-CODE_CHANGE_PATTERN = [
-    "Code Change",
-    "Code Changes",
-    "Code Diff",
-    "Commit Diff",
-]
 FIRST_LEVEL_KEYWORDS = {
-    "Vulnerabilities": SOFTWARE_VUL_PATTERN,
-    "Verification": SOFTWARE_VERIFY_PATTERN,
-    "Smart Contracts": SMART_CONTRACTS_PATTERN,
-    "Unit Test": UNIT_TEST_PATTERN,
-    "Automated Program Repair": APR_PATTERN,
-    "Large Language Models": LLMS_PATTERN,
-    "Fuzzing": FUZZING_PATTERN,
-    "Code Review": CODE_REVIEW_PATTERN,
-    "Continuous Integration": CI_PATTERN,
-    "Fault Localization": FL_PATTERN,
-    "Code": CODE_PATTERN,
-    "Bug": BUG_PATTERN,
-    "Commit Message": COMMIT_MESSAGE_PATTERN,
-    "Code Change": CODE_CHANGE_PATTERN,
+    "Vulnerabilities": [
+        "Software Vulnerabilities",
+        "Vulnerabilities",
+        "Vulnerability",
+    ],
+    "Verification": [
+        "Formal Verification",
+        "Verification",
+    ],
+    "Smart Contracts": [
+        "Smart Contracts",
+        "Smart Contract",
+    ],
+    "Unit Test": [
+        "Unit Test"
+    ],
+    "APR": [
+        "Automated Program Repair",
+        "Program Repair",
+        "Automated Repair",
+    ],
+    "LLM": [
+        "LLMs",
+        "LLM",
+        "Large Language Models",
+        "Large Language Model",
+        "ChatGPT",
+        "GPT",
+        "LLaMA"
+    ],
+    "Fuzzing": [
+        "Fuzzing",
+        "Fuzz",
+    ],
+    "Code Review": [
+        "Code Review",
+    ],
+    "Continuous Integration": [
+        "Continuous Integration"
+    ],
+    "Fault Localization": [
+        "Fault Localization"
+    ],
+    "Code": [
+        "Code"
+    ],
+    "Bug": [
+        "Bug"
+    ],
+    "Commit Message": [
+        "Commit Message",
+        "Commit Messages",
+    ],
+    "Code Change": [
+        "Code Change",
+        "Code Changes",
+        "Code Diff",
+        "Commit Diff",
+    ],
+    "Software Testing": [
+        "Software Test",
+    ],
+    "Ethereum": [
+        "Ethereum",
+    ],
+    "Static Analysis": [
+        "Static Analysis",
+        "SAST",
+        "Code Property Graph",
+        "Control Flow Graph",
+        "Data Flow",
+        "Call Graph"
+    ],
 }
 
-DETECTION_PATTERN = [
-    "Detector",
-    "Identifying",
-    "Detecting",
-    "Catching",
-    "Detection",
-]
-REPAIR_PATTERN = [
-    "Repair",
-    "Repairing",
-]
-GENERATION_PATTERN = [
-    "Generation",
-    "Completion",
-    "Translation",
-]
-AGENT_PATTERN = [
-    "Agent",
-    "Agents",
-]
-EXPLOIT_PATTERN = [
-    "Exploit",
-    "Exploits",
-]
 SECOND_LEVEL_KEYWORDS = {
-    "Detection": DETECTION_PATTERN,
-    "Repair": REPAIR_PATTERN,
-    "Generation": GENERATION_PATTERN,
-    "Agent": AGENT_PATTERN,
-    "Exploit": EXPLOIT_PATTERN,
+    "Detection": [
+        "Detector",
+        "Identifying",
+        "Detecting",
+        "Catching",
+        "Detection",
+    ],
+    "Repair": [
+        "Repair",
+        "Repairing",
+    ],
+    "Generation": [
+        "Generation",
+        "Completion",
+        "Translation",
+    ],
+    "Agent": [
+        "Agent",
+        "Agents",
+    ],
+    "Exploit": [
+        "Exploit",
+        "Exploits",
+    ],
 }
